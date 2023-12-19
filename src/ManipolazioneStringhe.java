25dc54cf4ff9d467a45a1a94500f521d049e3dea
Rodrigo Sebastian Montero Mozombite <rodrigo.montero@itsrizzoli.it>
2023-12-19 09:34:28 +0100

    metodo maiuscolo creato

--- src/ManipolazioneStringhe.java
+++ src/ManipolazioneStringhe.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ManipolazioneStringhe {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Inserisci una stringa: ");
+        String inputUtente = scanner.nextLine();
+
+        System.out.println("Maiuscolo: " + maiuscolo(inputUtente));
+        scanner.close();
+
+        
+    }
+
+    public static String maiuscolo(String str){
+        return str.toUpperCase();
     }
 }
